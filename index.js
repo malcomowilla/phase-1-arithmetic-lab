39aa026dc8385edde302dc4dcd63e59e0c0d81e2
malcomowilla <owillamalcom@gmail.com>
2023-04-14 18:47:23 +0300

    solved

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
 
-// Write your code here
+// // Write your code here
+
+// const multiply = 0.1 * 0.1
+
+// const add = 0.1 + 0.1 + 0.1
+// const power = 2 ** 8
+// console.log(multiply)
+// console.log(add)
+// console.log(power)
+
+// // let counter = 0;
+
+// // ++counter
+// // ++counter
+// // console.log(counter)
+
+// const number = 2
+// const num1 = 4
+// const num2 = 0.42
+// const string = '100'
+// // console.log(Number.isInteger(number))
+// // console.log(Number.isInteger(num1))
+// // console.log(Number.isInteger(num2))
+// // console.log(Number.parseInt(string, 2))
+// console.log(Math.min(1, 2, 3, 4, 5))
+
+
+
+
+// console.log(Math.random())
+// console.log(Math.ceil(Math.random() * 10) + 1)
+// console.log(Math.floor(Math.random() *10))
+
+// console.log(Math.random() * 10)
+
+// console.log(Math.floor(Math.random() * 9 ))
+
+// console.log(Math.ceil(7.7543))
+
+// function multiplyNumber(num1, num2) {
+//      num1 * num2
+// }
+
+// const multiply = multiplyNumber()
+
+const num1 = 62
+const num2 = 1
+
+const multiply = num1 * num2
+const random = Math.ceil(Math.random())
+
+const num3 = 22
+const num4 = 6
+const mod = num3 % num4
+const max = Math.max(1, 2, 3, 13, 20, 12)
